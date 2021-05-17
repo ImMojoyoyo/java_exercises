9f0e741460c2544f5ae29a434187c53f14d6bb23
ImMojoyoyo <towwwnsville@gmail.com>
2021-05-17 21:28:41 +0200

    First

---
+++ java_exercises/src/01/hello_world.java
@@ -0,0 +1,7 @@
+public class hello_world {
+    // Programa hola mundo
+    public static void main(String[] args) throws Exception {
+        // Este programa muestra un saludo por la pantalla.
+        System.out.println("Hello, Sergi. I'm your first program with JAVA");
+    }
+}
---
+++ java_exercises/src/02/algebra.java
@@ -0,0 +1,8 @@
+public class algebra {
+    public static void main(String[] args ){
+        int a = 1, b = 2, c = 3;
+        int d = 4 , e= 6;
+        double resultado1 = d / b * c / e + e / b/ a / d * b;
+        System.out.print(resultado1);
+    }
+}
